febb5d7c135e10537b97160d02845e012283f3dd
zaqijr7 <zaqijr7@gmail.com>
2021-02-28 21:46:47 +0700

    create splash screen

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import NavigationScreenRoot from './src/navigation';
 import {NavigationContainer} from '@react-navigation/native';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import persistedStore from './src/redux/store';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
   const {persistor, store} = persistedStore();
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <PersistGate persistor={persistor}>
       <Provider store={store}>
--- android/app/src/main/java/com/tickitzmobile/MainApplication.java
+++ android/app/src/main/java/com/tickitzmobile/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.reactnativecommunity.picker.RNCPickerPackage;
 import com.reactcommunity.rndatetimepicker.RNDateTimePickerPackage;
